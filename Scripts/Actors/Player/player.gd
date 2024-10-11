5f6ca44297549a1d7e4fc48a1db27429c4e23403
Pierre Bielen <godot.tuto.fr@gmail.com>
2024-10-11 12:54:22 +0200

    player animation

--- Scripts/Actors/Player/player.gd
+++ Scripts/Actors/Player/player.gd
@@ -1,4 +1,6 @@
 class_name Player extends CharacterBody2D
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 
 #const SPEED:float = 130
 @export var Speed:float = 130;
@@ -7,7 +9,13 @@ var direction:float = 0
 
 func _input(event: InputEvent) -> void:
 	direction = Input.get_axis("move_left","move_right")
-	print(direction)
+	# animation du player
+	if (direction < 0):
+		animated_sprite.play("Fly_left")
+	elif (direction > 0):
+		animated_sprite.play("Fly_right")
+	else :
+		animated_sprite.play("Fly_forward")
 
 func _physics_process(delta: float) -> void:
 	velocity.x = Speed * direction
