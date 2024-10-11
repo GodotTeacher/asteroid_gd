d3dc984a2c49e98854434474e3b579bc14e9747f
Pierre Bielen <godot.tuto.fr@gmail.com>
2024-10-11 14:17:31 +0200

    Teleporting

--- Scripts/Actors/Player/player.gd
+++ Scripts/Actors/Player/player.gd
@@ -7,6 +7,7 @@ class_name Player extends CharacterBody2D
 
 var direction:float = 0
 
+@warning_ignore("unused_parameter")
 func _input(event: InputEvent) -> void:
 	direction = Input.get_axis("move_left","move_right")
 	# animation du player
@@ -17,6 +18,16 @@ func _input(event: InputEvent) -> void:
 	else :
 		animated_sprite.play("Fly_forward")
 
+@warning_ignore("unused_parameter")
 func _physics_process(delta: float) -> void:
 	velocity.x = Speed * direction
 	move_and_slide()
+	# Teleportation
+	var screen_width:float = get_viewport_rect().size.x
+	if(position.x < 0):
+		# Translation du player a droite
+		position.x = screen_width
+	if(position.x > screen_width):
+		# Translation du player a gauche
+		position.x = 0
+	
