8a181cccbe758c54a85dfa6568bbb776be603bff
Pierre Bielen <godot.tuto.fr@gmail.com>
2024-10-11 14:41:16 +0200

    laser spawn

--- Scripts/Actors/Player/player.gd
+++ Scripts/Actors/Player/player.gd
@@ -1,9 +1,12 @@
 class_name Player extends CharacterBody2D
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var marker_left: Marker2D = $MarkerLeft
+@onready var marker_right: Marker2D = $MarkerRight
 
 
 #const SPEED:float = 130
 @export var Speed:float = 130;
+@export var Laser_scene:PackedScene;
 
 var direction:float = 0
 
@@ -17,6 +20,10 @@ func _input(event: InputEvent) -> void:
 		animated_sprite.play("Fly_right")
 	else :
 		animated_sprite.play("Fly_forward")
+		
+	# pondre les lasers
+	if(Input.is_action_just_pressed("shoot")):
+		spawn_lasers()
 
 @warning_ignore("unused_parameter")
 func _physics_process(delta: float) -> void:
@@ -31,3 +38,11 @@ func _physics_process(delta: float) -> void:
 		# Translation du player a gauche
 		position.x = 0
 	
+func spawn_lasers()->void:
+	var laserL = Laser_scene.instantiate()
+	var laserR = Laser_scene.instantiate()
+	
+	laserL.position = marker_left.position
+	laserR.position = marker_right.position
+	add_child(laserL)
+	add_child(laserR)
