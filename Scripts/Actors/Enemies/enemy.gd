480fc7cf91ae4db2e75f45ec2fcfc915c2f3aba7
Pierre Bielen <godot.tuto.fr@gmail.com>
2024-10-11 16:00:47 +0200

    enemy destroy

---
+++ Scripts/Actors/Enemies/enemy.gd
@@ -0,0 +1,14 @@
+class_name Enemy extends CharacterBody2D
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+@export var Speed:float = 80
+
+func _physics_process(delta: float) -> void:
+	velocity.y = Speed;
+	move_and_slide()
+
+func detroy_enemy()-> void:
+	animated_sprite.play("Explode")
+
+func _on_animation_finished() -> void:
+	queue_free()
--- Scripts/Actors/Player/laser.gd
+++ Scripts/Actors/Player/laser.gd
@@ -1,7 +1,16 @@
 class_name Laser extends Area2D
+
+
 @export var Speed:float = 150
 func _physics_process(delta: float) -> void:
 	position.y -= Speed * delta
 
 func _on_laser_screen_exited() -> void:
 	queue_free()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	(body as Enemy).detroy_enemy()
+
+
+	
