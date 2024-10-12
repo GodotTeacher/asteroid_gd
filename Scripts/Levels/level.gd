d4a304711fef8e462126a76d7ba5d70528515da8
GodotTeacher <godot.tuto.fr@gmail.com>
2024-10-12 14:03:24 +0200

    disable physics process

--- Scripts/Levels/level.gd
+++ Scripts/Levels/level.gd
@@ -2,6 +2,11 @@ extends Node2D
 @export var Enemy_scenes:Array[PackedScene]
 @export var Player:Player
 @export var Spawn_timer:Timer
+
+@onready var enemies_container: Node2D = $EnemiesContainer
+
+
+
 var score : int = 0
 
 func _ready() -> void:
@@ -14,7 +19,7 @@ func _on_spawn_timer_timeout() -> void:
 	var  new_enemy:Enemy = Enemy_scenes[index].instantiate()
 	new_enemy.on_enemy_destroyed.connect(_on_enemy_destroyed)
 	new_enemy.position = Vector2(pos_x,-30)
-	add_child(new_enemy)
+	enemies_container.add_child(new_enemy)
 
 func _on_enemy_destroyed(enemy : Enemy)-> void:
 	score += 1
@@ -22,4 +27,7 @@ func _on_enemy_destroyed(enemy : Enemy)-> void:
 	
 func _on_player_died(player: Player):
 	Spawn_timer.stop()
+	for e in enemies_container.get_children():
+		e.set_physics_process(false)
+	  
 	print("Player died")
